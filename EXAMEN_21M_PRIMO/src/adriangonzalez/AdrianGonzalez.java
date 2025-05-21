86995bea1a0ca3c49e9cd62c14329e4c8c900a73
Ivan <Ivan@RUBEN>
2025-05-21 11:29:44 +0200

    Creada la rama master con el código

---
+++ EXAMEN_21M_PRIMO/src/adriangonzalez/AdrianGonzalez.java
@@ -0,0 +1,12 @@
+package adriangonzalez;
+
+import java.util.Scanner;
+
+public class AdrianGonzalez {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+        System.out.println("Buenos días Ceinmark, soy Marta Gómez");
+        System.out.print("Introduce un número para averiguar si es primo o no: ");
+        int numero = entrada.nextInt();
+	}
+}
